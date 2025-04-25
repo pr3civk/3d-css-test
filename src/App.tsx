bbe8e8293d37e94329b02da0d47f2051224b7120
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2025-04-25 17:37:25 +0200

    Add classnames and vite-plugin-svgr dependencies; update Vite config and App component for SVG handling and animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,147 +1,169 @@
-import { 
-  Aperture, 
-  Atom, 
-  Binary, 
-  Brain, 
-  BrainCircuit as Circuit, 
-  Cpu, 
-  Database, 
-  Globe, 
-  Hexagon, 
-  Network  
-} from 'lucide-react';
-import { motion } from 'motion/react';
-import { useEffect, useRef, useState } from 'react';
+import { motion, useAnimationFrame } from 'motion/react'
+import { useEffect, useRef, useState } from 'react'
+import cn from 'classnames'
+// import XIcon from './assets/x-icon.svg?react'
+import { partnerIcons } from './partner-icons'
 
-const logos = [
-  { Icon: Aperture, color: 'text-white opacity-90' },
-  { Icon: Atom, color: 'text-white opacity-80' },
-  { Icon: Binary, color: 'text-white opacity-85' },
-  { Icon: Brain, color: 'text-white opacity-90' },
-  { Icon: Circuit, color: 'text-white opacity-80' },
-  { Icon: Cpu, color: 'text-white opacity-85' },
-  { Icon: Database, color: 'text-white opacity-90' },
-  { Icon: Globe, color: 'text-white opacity-85' },
-  { Icon: Hexagon, color: 'text-white opacity-80' },
-  { Icon: Network, color: 'text-white opacity-90' }
-];
-
-const variants = {
-  initial: {
-    rotateY: 0
-  },
-  animate: {
-    rotateY: 360,
-    transition: {
-      duration: 30,
-      ease: "linear",
-      repeat: Infinity
-    }
-  },
-  exit: {
-    rotateY: 720,
-    transition: {
-      duration: 2,
-      ease: "easeInOut",
-      repeat: Infinity
-
-    }
-  }
+const defaultVariants = {
+  initial: { opacity: 0 },
+  animate: ({ isFinished, maxOpacity }: { isFinished: boolean; maxOpacity: number }) => ({
+    opacity: isFinished ? [0, maxOpacity] : undefined,
+    transition: { duration: 1.5, ease: [0.22, 0.65, 0.36, 1] },
+  }),
 }
+
 function App() {
   const [isLeaving, setIsLeaving] = useState(false)
-  const [opacity, setOpacity] = useState(1)
-  const ref = useRef<HTMLDivElement>(null)
+  const [isFinished, setIsFinished] = useState(false)
+  const [isOpacityZero, setIsOpacityZero] = useState(false)
+
+  const containerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (!ref.current || opacity === 0) return
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === 'style') {
-          const currentOpacity = window.getComputedStyle(ref.current!).opacity
-          setOpacity(Number(currentOpacity))
+    if (!isLeaving) return
+    const checkOpacity = () => {
+      if (containerRef.current) {
+        const opacity = window.getComputedStyle(containerRef.current).opacity
+
+        if (opacity === '0') {
+          setIsOpacityZero(true)
+        }
+      }
+    }
+
+    const timeoutId = setTimeout(checkOpacity, 600)
+
+    return () => clearTimeout(timeoutId)
+  }, [isLeaving])
+
+  const carouselRef = useRef<HTMLDivElement>(null)
+
+  const iconRefs = useRef<(HTMLDivElement | null)[]>([])
+
+  useAnimationFrame((time) => {
+    if (carouselRef.current) {
+      const angle = time * 0.01
+      partnerIcons.forEach((_, index) => {
+        const iconElement = iconRefs.current[index]
+        if (iconElement) {
+          const baseAngle = ((index + 1) / partnerIcons.length) * 360
+          const currentAngle = (baseAngle + angle) % 360
+          const angleInRadians = (currentAngle * Math.PI) / 180
+
+          const radiusX = window.innerWidth > 1600 ? 700 : 550
+          const radiusY = window.innerWidth > 1600 ? 425 : 300
+
+          const x = Math.sin(angleInRadians) * radiusX
+          const y = -Math.cos(angleInRadians) * radiusY - 30
+
+          const scale = Math.max(1, Math.min(1.5, 0.5 + Math.sin(angleInRadians) * 0.5))
+
+          iconElement.style.transform = `translate(${x}px, ${y}px) rotateY(${
+            Math.cos(angleInRadians) / 2
+          }rad) scale(${scale})`
         }
       })
-    })
+    }
+  })
 
-    observer.observe(ref.current, { attributes: true })
-    return () => observer.disconnect()
-  }, [opacity])
-
-  if (opacity === 0) {
-    return (
-      <div>
-        <h1>Leaving</h1>
-      </div>
-    )
+  if (isOpacityZero) {
+    return null
   }
 
   return (
-    <div className="fixed inset-0 bg-black flex items-center justify-center overflow-hidden">
-      <MovieAnimation />
-      <div className="relative w-screen h-screen perspective-1000 -rotate-x-[30deg]  transform-style-3d">
+    <motion.div
+      ref={containerRef}
+      className="fixed inset-0 bg-black flex items-center justify-center overflow-hidden"
+      initial={{ opacity: 1 }}
+      animate={{
+        opacity: isLeaving ? 0 : 1,
+        maskImage: isLeaving ? 'linear-gradient(to bottom, #8c8c8c, transparent)' : 'none',
+      }}
+      transition={{ duration: 1.25, ease: [0.22, 0.65, 0.36, 1] }}
+    >
+      <MovieAnimation {...{ isFinished, setIsFinished }} />
+      <div className="relative w-screen h-screen perspective-1000 -rotate-x-[30deg]">
         <motion.div
-          className="absolute w-full h-full transform-style-3d"
-          variants={variants}
+          variants={defaultVariants}
           initial="initial"
-          animate={isLeaving ? 'exit' : 'animate'}
-        >
-          {logos.map((Logo, index) => {
-            const angle = (index / logos.length) * 360
+          animate={isFinished ? 'animate' : 'initial'}
+          custom={{ isFinished, maxOpacity: 0.11 }}
+          className="size-[60svw] bg-white rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 blur-3xl"
+          style={{
+            maskImage: 'radial-gradient(circle, #ffffff 21.37%, transparent 75%)',
+          }}
+        />
+
+        <div className="absolute w-full h-full" ref={carouselRef}>
+          {partnerIcons.map((Logo, index) => {
+            if (!iconRefs.current[index]) {
+              iconRefs.current[index] = null
+            }
+
             return (
               <motion.div
-                ref={ref}
-                initial={{ opacity: 1 }}
-                animate={isLeaving && { opacity: [1, 0] }}
-                transition={{
-                  duration: 1.25,
-                  ease: 'easeInOut',
-                }}
+                initial={{ opacity: 0 }}
+                animate={
+                  isFinished
+                    ? { opacity: 1, transition: { duration: 1.25, ease: [0.22, 0.65, 0.36, 1] } }
+                    : { opacity: 0 }
+                }
                 key={index}
                 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 backdrop-blur-2xl"
-                style={{
-                  transform: `
-                  rotateY(${angle}deg) 
-                  translateX(${window.innerWidth > 1600 ? 800 : 600}px)
-                  translateY(-150px) 
-                `,
+                ref={(el) => {
+                  iconRefs.current[index] = el
+                  return undefined
                 }}
               >
-                <div
-                  className={`w-32 h-32 rounded-full p-6 origin-center
-                           hover:scale-110 transition-all duration-300
-                           flex items-center justify-center 
-                           backdrop-blur-sm bg-gradient-to-br from-white/10 to-transparent
-                           shadow-lg shadow-white/5 transform-style-3d 
-                           opacity-100 hover:bg-white group
-                           `}
-                >
-                  <Logo.Icon className={`w-20 h-20 group-hover:text-black transition-all duration-300 ${Logo.color}`} />
-                </div>
+                <Logo.icon className="bg-transparent max-w-36" />
               </motion.div>
             )
           })}
-        </motion.div>
+        </div>
       </div>
-      <button
-        className="cursor-pointer absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 backdrop-blur-sm px-4 py-2 rounded-full z-20 bg-red-300"
-        onClick={() => setIsLeaving(true)}
+      <motion.div
+        variants={defaultVariants}
+        initial="initial"
+        animate={isFinished ? 'animate' : 'initial'}
+        custom={{ isFinished, maxOpacity: 1 }}
+        className="absolute bottom-[30%] left-1/2 -translate-x-1/2 z-30 flex flex-col items-center gap-12"
       >
-        Leave
-      </button>
-    </div>
+        <div className="font-semibold text-center space-y-1">
+          <p className="text-4xl block text-white">Welcome to StackR</p>
+          <p className="text-4xl block bg-gradient-to-tr from-[#fff] to-[#8b8b8b] bg-clip-text text-transparent">
+            The new home for your collectibles
+          </p>
+        </div>
+        <button
+          onClick={() => setIsLeaving(true)}
+          className="font-semibold relative inline-flex h-14 overflow-hidden p-[2px] rounded-xl"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_4s_linear_infinite] rounded-xl bg-[conic-gradient(from_90deg_at_50%_50%,#4978CF_0%,#760396_50%,#4978CF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-black px-8 py-2 text-lg font-medium text-white backdrop-blur-3xl uppercase">
+            get started
+          </span>
+        </button>
+      </motion.div>
+    </motion.div>
   )
 }
-const MovieAnimation = () => {
+
+const MovieAnimation = ({
+  isFinished,
+  setIsFinished,
+}: {
+  isFinished: boolean
+  setIsFinished: (isFinished: boolean) => void
+}) => {
   const videoRef = useRef<HTMLVideoElement>(null)
-  const [isFinished, setIsFinished] = useState(false)
 
   useEffect(() => {
+    if (isFinished) return
     const handleUserInteraction = () => {
       if (videoRef.current) {
         videoRef.current.muted = false
         videoRef.current.play().catch((err) => {
-          console.error('Nie udało się odtworzyć z dźwiękiem:', err)
+          console.error('Could not play video:', err)
         })
         document.removeEventListener('click', handleUserInteraction)
       }
@@ -161,14 +183,15 @@ const MovieAnimation = () => {
     return () => {
       document.removeEventListener('click', handleUserInteraction)
     }
-  }, [])
-
-  if (isFinished) {
-    return
-  }
+  }, [isFinished, setIsFinished])
 
   return (
-    <div className="fixed inset-0 bg-black flex items-center justify-center overflow-hidden z-40">
+    <div
+      className={cn(
+        'fixed inset-0 bg-black flex items-center justify-center overflow-hidden',
+        isFinished ? 'z-0 pointer-events-none' : 'z-[9999]',
+      )}
+    >
       <video
         ref={videoRef}
         autoPlay
@@ -179,7 +202,7 @@ const MovieAnimation = () => {
         className="w-full h-full object-cover"
       >
         <source
-          src="https://cjn1foix8guknqhi.public.blob.vercel-storage.com/logo-stackr-gDfX4Jlux4pEuMyhJFim2Jh5C6VsXz.mp4"
+          src="https://cjn1foix8guknqhi.public.blob.vercel-storage.com/FinalComp_nobg-vib9lPO0UxD84mR7qVotx71tUJY9TH.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
@@ -188,4 +211,73 @@ const MovieAnimation = () => {
   )
 }
 
-export default App;
+export const BackgroundGradient = ({
+  children,
+  className,
+  containerClassName,
+  animate = true,
+}: {
+  children?: React.ReactNode
+  className?: string
+  containerClassName?: string
+  animate?: boolean
+}) => {
+  const variants = {
+    initial: {
+      backgroundPosition: '0 50%',
+    },
+    animate: {
+      backgroundPosition: ['0, 50%', '100% 50%', '0 50%'],
+    },
+  }
+  return (
+    <div className={cn('relative p-[4px] group rounded-full', containerClassName)}>
+      <motion.div
+        variants={animate ? variants : undefined}
+        initial={animate ? 'initial' : undefined}
+        animate={animate ? 'animate' : undefined}
+        transition={
+          animate
+            ? {
+                duration: 5,
+                repeat: Infinity,
+                repeatType: 'reverse',
+              }
+            : undefined
+        }
+        style={{
+          backgroundSize: animate ? '200% 200%' : undefined,
+        }}
+        className={cn(
+          'absolute inset-0 rounded-3xl z-[1] opacity-60 group-hover:opacity-100 blur-xl transition duration-500 will-change-transform',
+          ' bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]',
+        )}
+      />
+      <motion.div
+        variants={animate ? variants : undefined}
+        initial={animate ? 'initial' : undefined}
+        animate={animate ? 'animate' : undefined}
+        transition={
+          animate
+            ? {
+                duration: 5,
+                repeat: Infinity,
+                repeatType: 'reverse',
+              }
+            : undefined
+        }
+        style={{
+          backgroundSize: animate ? '400% 400%' : undefined,
+        }}
+        className={cn(
+          'absolute inset-0 rounded-3xl z-[1] will-change-transform',
+          'bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]',
+        )}
+      />
+
+      <div className={cn('relative z-50', className)}>{children}</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/partner-icons.ts
@@ -0,0 +1,25 @@
+import Logo1 from './assets/partners/1.svg?react'
+import Logo2 from './assets/partners/2.svg?react'
+import Logo3 from './assets/partners/3.svg?react'
+import Logo4 from './assets/partners/4.svg?react'
+import Logo5 from './assets/partners/5.svg?react'
+import Logo6 from './assets/partners/6.svg?react'
+import Logo7 from './assets/partners/7.svg?react'
+import Logo8 from './assets/partners/8.svg?react'
+import Logo9 from './assets/partners/9.svg?react'
+import Logo10 from './assets/partners/10.svg?react'
+import Logo11 from './assets/partners/11.svg?react'
+
+export const partnerIcons = [
+  { name: 'Logo1', icon: Logo1 },
+  { name: 'Logo2', icon: Logo2 },
+  { name: 'Logo3', icon: Logo3 },
+  { name: 'Logo4', icon: Logo4 },
+  { name: 'Logo5', icon: Logo5 },
+  { name: 'Logo6', icon: Logo6 },
+  { name: 'Logo7', icon: Logo7 },
+  { name: 'Logo8', icon: Logo8 },
+  { name: 'Logo9', icon: Logo9 },
+  { name: 'Logo10', icon: Logo10 },
+  { name: 'Logo11', icon: Logo11 },
+]
